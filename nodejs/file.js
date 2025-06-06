72a5789c7f9af82abb347633e67ebaa8c0eba779
Nitish sahni <nitish@realmeta.ca>
2025-06-06 15:07:45 -0300

    Async&SyncOperation

--- nodejs/file.js
+++ nodejs/file.js
@@ -1,9 +1,17 @@
 const fs = require("fs");
+const os = require("os");
 
 
 //SYNC.....
 //fs.writeFileSync("./test.txt" , "Hey their , this file is created by FS Module.");
-
+console.log(1);
 //Async
-const result = fs.writeFile("./test.txt" ,"hello world Async" , (err)=>{} );
-console.log(result);
+fs.readFile("./test.txt" ,"utf-8" , (err,result)=>{
+    console.log(result);
+
+} );
+
+console.log(2);
+console.log(3);
+console.log(4);
+console.log(5);
