9565ba01bb209a4812bf7b3dfb997cd4500533be
Nitish sahni <nitish@realmeta.ca>
2025-06-12 19:23:15 -0300

    https protocol

--- SERVER/index.js
+++ SERVER/index.js
@@ -1,16 +1,21 @@
 const http = require("http");
 const fs = require("fs");
+const url = require("url");
 
 const myServer = http.createServer((req , res) =>{
     const log = `${Date.now()}: ${req.url} New Req Received\n`
+    const myUrl = url.parse(req.url , true);
+    console.log(myUrl);
     fs.appendFile("log.txt" , log , (err , data)=>{
-        switch(req.url){
+        switch(myUrl.pathname
+        ){
             case "/":
             res.end("homepage");
             break;
 
             case "/about":
-            res.end("Hi My name is Nitish Sahni");
+                const username = myUrl.query.myname;
+            res.end(`Hi MR. ${username}`);
             break;
 
             default:
